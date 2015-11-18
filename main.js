ac14984da186966660333954319509d6f84ecd50
davidmccaslin <david.mccaslin@outlook.com>
2015-11-18 16:17:56 -0700

    More updates

--- main.js
+++ main.js
@@ -3,34 +3,59 @@ $().ready(function () {
         {
             containment: "parent",
             drag: function(event, ui) {
-                $("#bottomOffsetTop").text(ui.offset.top);
-                $("#bottomOffsetLeft").text(ui.offset.left);
-                $("#bottomPosistionTop").text(ui.position.top);
-                $("#bottomPosistionLeft").text(ui.position.left);
 
+                var bottomScrollBarContainer = $(".scrollBar.scrollBarBottom");
                 var content = $(".content");
+
+                var contentWidth = content.width();
+                var bottomScrollBarContainerSize = bottomScrollBarContainer.width();
+
+                var contentRatio = 1 - (bottomScrollBarContainerSize/contentWidth);
+
                 var currentPosistion = content.css("left");
                 var positionChange = ui.position.left - lastBottomScrollBarPosition 
                 lastBottomScrollBarPosition = ui.position.left;
 
-                content.css("left", "+=" + positionChange);
+                content.css("left", "-=" + positionChange * contentRatio);
+
+                $("#contentPosistionTop").text(content.css("top"));
+                $("#contentPosistionLeft").text(content.css("left"));
+                $("#contentRatioBottom").text(contentRatio.toFixed(2) + "%");
+
+                $("#bottomOffsetTop").text(ui.offset.top.toFixed(2));
+                $("#bottomOffsetLeft").text(ui.offset.left.toFixed(2));
+                $("#bottomPosistionTop").text(ui.position.top.toFixed(2));
+                $("#bottomPosistionLeft").text(ui.position.left.toFixed(2));
             }
         }); 
     $(".scrollBar.rightScrollBlock").draggable(
         {
             containment: "parent",
             drag: function(event, ui) {
-                $("#rightOffsetTop").text(ui.offset.top);
-                $("#rightOffsetLeft").text(ui.offset.left);
-                $("#rightPosistionTop").text(ui.position.top);
-                $("#rightPosistionLeft").text(ui.position.left);
-
+                var rightScrollBarContainer = $(".scrollBar.scrollBarRight");
                 var content = $(".content");
+                var viewPort = $(".viewPort");
+                
+                var viewPortHeight = viewPort.height();
+                var contentHeight = content.height();
+                var rightScrollBarContainerSize = rightScrollBarContainer.height();
+                
+                var contentRatio = (rightScrollBarContainerSize/(contentHeight - viewPortHeight));
+                
                 var currentPosistion = content.css("left");
                 var positionChange = ui.position.top - lastRightScrollBarPosition 
                 lastRightScrollBarPosition = ui.position.top;
 
-                content.css("top", "+=" + positionChange);
+                content.css("top", "-=" + positionChange * contentRatio);
+
+                $("#contentPosistionTop").text(content.css("top"));
+                $("#contentPosistionLeft").text(content.css("left"));
+                $("#contentRatioRight").text(contentRatio.toFixed(2) + "%");
+
+                $("#rightOffsetTop").text(ui.offset.top.toFixed(2));
+                $("#rightOffsetLeft").text(ui.offset.left.toFixed(2));
+                $("#rightPosistionTop").text(ui.position.top.toFixed(2));
+                $("#rightPosistionLeft").text(ui.position.left.toFixed(2));
             }}); 
 });
 
